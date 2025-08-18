ddbc7753fd6f631d69b0cc733ac32b0ff7c2ddd3
mimasekaric <mimasekaric@gmail.com>
2025-08-18 18:54:35 +0200

    Working with views

--- app/src/main/java/com/example/myhobitapplication/RegisterScreen.java
+++ app/src/main/java/com/example/myhobitapplication/RegisterScreen.java
@@ -1,6 +1,9 @@
 package com.example.myhobitapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,13 +19,16 @@ public class RegisterScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button regButton = findViewById(R.id.etRegButton);
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterScreen.this,HomeScreen.class);
+                startActivity(intent);
+            }
         });
-
-
     }
 
     @Override
