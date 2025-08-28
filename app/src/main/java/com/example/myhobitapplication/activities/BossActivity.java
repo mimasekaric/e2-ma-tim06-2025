4825fda0c3aa58121f7eabaee09530b812de0272
mimasekaric <mimasekaric@gmail.com>
2025-08-28 22:23:01 +0200

    Added animation for boss

--- app/src/main/java/com/example/myhobitapplication/activities/BossActivity.java
+++ app/src/main/java/com/example/myhobitapplication/activities/BossActivity.java
@@ -1,10 +1,33 @@
 package com.example.myhobitapplication.activities;
 
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myhobitapplication.R;
+import com.example.myhobitapplication.databinding.ActivityBossBinding;
 
 public class BossActivity extends AppCompatActivity {
 
+    private ActivityBossBinding binding;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityBossBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ImageView myImage = binding.myAnimatedImage;
+
+        Animation moveUpDown = AnimationUtils.loadAnimation(this, R.anim.dragon_up_down);
+
+        myImage.startAnimation(moveUpDown);
+    }
     
 
 }
