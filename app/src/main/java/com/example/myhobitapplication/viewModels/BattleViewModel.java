429a7a163c62ff3a4299ebd538d7184b824539fa
mimasekaric <mimasekaric@gmail.com>
2025-09-15 23:51:51 +0200

    Equipment reward after defeating boss
    - without ui

--- app/src/main/java/com/example/myhobitapplication/viewModels/BattleViewModel.java
+++ app/src/main/java/com/example/myhobitapplication/viewModels/BattleViewModel.java
@@ -1,5 +1,7 @@
 package com.example.myhobitapplication.viewModels;
 
+import android.widget.Toast;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -11,21 +13,26 @@ import com.example.myhobitapplication.dto.BossDTO;
 import com.example.myhobitapplication.dto.UserEquipmentDTO;
 import com.example.myhobitapplication.enums.ClothingTypes;
 import com.example.myhobitapplication.enums.EquipmentTypes;
+import com.example.myhobitapplication.enums.WeaponTypes;
 import com.example.myhobitapplication.models.Boss;
 import com.example.myhobitapplication.models.Clothing;
+import com.example.myhobitapplication.models.Equipment;
 import com.example.myhobitapplication.models.Profile;
 import com.example.myhobitapplication.models.UserEquipment;
+import com.example.myhobitapplication.models.Weapon;
 import com.example.myhobitapplication.services.BattleService;
 import com.example.myhobitapplication.services.BossService;
 import com.example.myhobitapplication.services.ProfileService;
 import com.example.myhobitapplication.services.TaskService;
 import com.example.myhobitapplication.services.UserEquipmentService;
+import com.example.myhobitapplication.staticData.WeaponList;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class BattleViewModel extends ViewModel {
     private final TaskService taskService;
@@ -48,22 +55,44 @@ public class BattleViewModel extends ViewModel {
     private final MutableLiveData<Boolean> _attackMissedEvent = new MutableLiveData<>();
     private final MutableLiveData<Profile> _userProfile = new MutableLiveData<>();
 
-    public double getHitChance() {return hitChance;}
-    public LiveData<Boolean> getAttackMissedEvent() { return _attackMissedEvent; }
+    public double getHitChance() {
+        return hitChance;
+    }
 
-    public LiveData<Integer> getBossCurrentHp() { return _bossCurrentHp; }
-    public LiveData<Integer> getBossMaxHp() { return _bossMaxHp; }
-    public LiveData<Integer> getUserPP() { return _userPP; }
-    public LiveData<Integer> getRemainingAttacks() { return _remainingAttacks; }
-    public LiveData<Boolean> getHitAnimationEvent() { return _hitAnimationEvent; }
+    public LiveData<Boolean> getAttackMissedEvent() {
+        return _attackMissedEvent;
+    }
+
+    public LiveData<Integer> getBossCurrentHp() {
+        return _bossCurrentHp;
+    }
+
+    public LiveData<Integer> getBossMaxHp() {
+        return _bossMaxHp;
+    }
+
+    public LiveData<Integer> getUserPP() {
+        return _userPP;
+    }
+
+    public LiveData<Integer> getRemainingAttacks() {
+        return _remainingAttacks;
+    }
+
+    public LiveData<Boolean> getHitAnimationEvent() {
+        return _hitAnimationEvent;
+    }
 
     private final MutableLiveData<Boolean> _isBattleOver = new MutableLiveData<>(false);
-    public LiveData<Boolean> isBattleOver() { return _isBattleOver; }
+
+    public LiveData<Boolean> isBattleOver() {
+        return _isBattleOver;
+    }
 
 
     private BossDTO currentBoss;
 
-    public BattleViewModel(TaskRepository taskRepository, BossRepository bossRepository, ProfileService profileService, UserEquipmentService userEquipmentService){
+    public BattleViewModel(TaskRepository taskRepository, BossRepository bossRepository, ProfileService profileService, UserEquipmentService userEquipmentService) {
         this.bossService = new BossService(bossRepository);
         this.profileService = profileService;
         this.taskService = new TaskService(taskRepository, profileService);
@@ -72,7 +101,7 @@ public class BattleViewModel extends ViewModel {
         userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
-    public void loadBattleState(String userId){
+    public void loadBattleState(String userId) {
 
         currentBoss = bossService.getLowestLevelBossForUser(userId);
 
@@ -85,25 +114,25 @@ public class BattleViewModel extends ViewModel {
         _bossMaxHp.setValue(currentBoss.getHP());
         _userPP.setValue(userPower);
         this.hitChance = 1.0;
+        // this.hitChance = battleService.calculateChanceForAttack(profile);
         //todo: moracu u bosu pamtiti koliko je ostalo pokusaja ubuduce?
         _remainingAttacks.setValue(5);
         //todo: moracu koristiti od usera datume levela prethodnog i sadanjeg i moracu povezati taskove sa userom - URADILA
         profileService.getProfileById(userUid).addOnSuccessListener(profile -> {
             userEquipmentService.activatedEquipmentEffect(profile);
             _userProfile.setValue(profile);
-            List<UserEquipmentDTO> ueList= userEquipmentService.getUserActivatedEquipment(profile.getuserUid());
-            for(UserEquipmentDTO u :  ueList){
-                if (u.getEquipment().getequipmentType().equals(EquipmentTypes.CLOTHING)){
-                    if(((Clothing)u.getEquipment()).getType().equals(ClothingTypes.SHIELD)){
-                        hitChance = hitChance + (hitChance * ((Clothing)u.getEquipment()).getpowerPercentage()/100);
+            List<UserEquipmentDTO> ueList = userEquipmentService.getUserActivatedEquipment(profile.getuserUid());
+            for (UserEquipmentDTO u : ueList) {
+                if (u.getEquipment().getequipmentType().equals(EquipmentTypes.CLOTHING)) {
+                    if (((Clothing) u.getEquipment()).getType().equals(ClothingTypes.SHIELD)) {
+                        hitChance = hitChance + (hitChance * ((Clothing) u.getEquipment()).getpowerPercentage() / 100);
                     }
-                    if(((Clothing)u.getEquipment()).getType().equals(ClothingTypes.BOOTS)){
+                    if (((Clothing) u.getEquipment()).getType().equals(ClothingTypes.BOOTS)) {
                         _remainingAttacks.setValue(_remainingAttacks.getValue() + 1);
                     }
                 }
             }
 
-       // this.hitChance = battleService.calculateChanceForAttack(profile);
 
         }).addOnFailureListener(e -> {
         });
@@ -124,7 +153,7 @@ public class BattleViewModel extends ViewModel {
 
         boolean isHitSuccessful = Math.random() <= this.hitChance;
 
-        if(isHitSuccessful) {
+        if (isHitSuccessful) {
 
             int newHp = currentHp - attackPower;
             if (newHp < 0) {
@@ -139,16 +168,16 @@ public class BattleViewModel extends ViewModel {
             if (isBossDefeated) {
                 currentBoss.setDefeated(true);
                 _isBattleOver.setValue(true);
-                userEquipmentService.incrementFightsCounter(userUid,_userProfile.getValue());
+                userEquipmentService.incrementFightsCounter(userUid, _userProfile.getValue());
                 battleService.rewardUserWithCoins(currentBoss);
+                rewardUserWithEquipment();
             }
 
 
             battleService.updateBoss(currentBoss);
 
             _hitAnimationEvent.setValue(true);
-        }
-        else{
+        } else {
             _attackMissedEvent.setValue(true);
         }
 
@@ -160,6 +189,79 @@ public class BattleViewModel extends ViewModel {
 
 
     }
+
+    public Equipment rewardUserWithEquipment() {
+
+        double randomChanceToGainEquipment = Math.random();
+        double randomChanceEquipment = Math.random();
+        double randomChanceEquipmentType = Math.random();
+        double chanceToGainEquipment = 0.2;
+        double weaponChance = 0.05;
+        ClothingTypes clothingTypes;
+        WeaponTypes weaponTypes;
+        //randomChanceToGainEquipment <= chanceToGainEquipment
+
+        if (true) {
+            if (randomChanceEquipment <= weaponChance) {
+                if (randomChanceEquipmentType < 0.333) {
+                    clothingTypes = ClothingTypes.GLOVES;
+                } else if (randomChanceEquipmentType < 0.666) {
+                    clothingTypes = ClothingTypes.SHIELD;
+                } else {
+                    clothingTypes = ClothingTypes.BOOTS;
+                }
+
+                List<Equipment> clothes = userEquipmentService.getByType(EquipmentTypes.CLOTHING);
+
+                Optional<Clothing> foundBoots = clothes.stream()
+                        .filter(item -> item instanceof Clothing)
+                        .map(item -> (Clothing) item)
+                        .filter(clothing -> clothing.getType() == clothingTypes)
+                        .findFirst();
+
+                if (foundBoots.isPresent()) {
+
+                    Clothing clothing = foundBoots.get();
+                    userEquipmentService.gainEquipment(userUid,clothing);
+                    return clothing;
+
+                } else {
+                   return null;
+                }
+
+            }
+            else {
+                if (randomChanceEquipmentType < 0.5) {
+                    weaponTypes = WeaponTypes.ANDURIL_OF_ARAGORN;
+                } else {
+                    weaponTypes = WeaponTypes.BOW_AND_ARROW_OF_LEGOLAS;
+                }
+                List<Equipment> weapons = userEquipmentService.getByType(EquipmentTypes.WEAPON);
+
+                Optional<Weapon> foundWeapon = weapons.stream()
+                        .filter(item -> item instanceof Weapon)
+                        .map(item -> (Weapon) item)
+                        .filter(clothing -> clothing.getType() == weaponTypes)
+                        .findFirst();
+
+                if (foundWeapon.isPresent()) {
+
+                    Weapon weapon = foundWeapon.get();
+                    userEquipmentService.gainEquipment(userUid,weapon);
+                    return weapon;
+
+                } else {
+                    return null;
+                }
+
+            }
+
+        }
+
+        return null;
+    }
+
+
 
     public void onAttackMissedEventHandled() {
         _attackMissedEvent.setValue(false);
